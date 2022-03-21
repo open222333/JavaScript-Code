baa76638b6bfaee474900e76f995ab798c71e547
Tom.Li <open222333@gmail.com>
2022-03-21 16:55:35 +0800

    20220321

--- MyExCode/JQuery/EX_html-form.js
+++ MyExCode/JQuery/EX_html-form.js
@@ -2,31 +2,36 @@
 
 /*
 範例
-
 <div class="btn-group">
 	<label class="btn btn-default btn-sm">
-		<form>
-			<span>一鍵修改域名</span>
-			<input type="text" id="changedomain_target" />
-			<input type="submit" class="btn btn-sm btn-success" value="提交" onclick="batchChangeDomain();"/>
+		<form id="batch_change_domain">
+			<span>一鍵修改域名  </span>
+			<span>舊域名:</span>
+			<input type="text" name="old_domain" />
+			<span>新域名:</span>
+			<input type="text" name="new_domain" />
+			<input type="submit" class="btn btn-sm btn-success" value="更換" />
 		</form>
 	</label>
 </div>
 */
 
-function batchChangeDomain() {
-  let targetDomain = document.getElementById('changedomain_target');
+$('#batch_change_domain').submit(function () {
+  let old_domain = $("input[name=old_domain]").val();
+  let new_domain = $("input[name=new_domain]").val();
   $.ajax({
     method: 'post',
     url: 'batch_change_domain',
     data: {
       _token: LA.token,
-      target_domain: targetDomain,
+      old_domain: old_domain,
+      new_domain: new_domain
     },
     success: function (response) {
       if (response['message'] == 'ok') {
         toastr.success('一鍵更換域名操作成功');
-        $.pjax.reload('#pjax-container');
+        // $.pjax.reload('#pjax-container');
+		
       }
       else {
         toastr.error(response['message']);
@@ -36,4 +41,4 @@ function batchChangeDomain() {
       toastr.error('一鍵更換域名操作失敗');
     },
   });
-}
+})
