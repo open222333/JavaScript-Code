877de39b7b08d14f89415ab1535a7c1e2a140c99
TomLi <open222333@gmail.com>
2023-07-06 10:07:53 +0800

    20230706

--- JavaScript_The_Definitive_Guide/ch13-Asynchronous/ch13-3-2.js
+++ JavaScript_The_Definitive_Guide/ch13-Asynchronous/ch13-3-2.js
@@ -1,4 +1,12 @@
 // async函式
+function getHighScore() {
+  return getJSON("/api/user/profile").then((profile) => profile.highScore);
+}
+
+// 修改成
+
 async function getHighScore() {
-  
+  let response = await fetch("/api/user/profile");
+  let profile = await response.json();
+  return profile.highScore;
 }
